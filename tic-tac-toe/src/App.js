be6b4f0e133fc1a03df9d3e1d5cd8f5f66d87141
singavarapur <rs28795n@pace.edu>
2024-10-15 21:43:24 -0400

    step16

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -8,12 +8,38 @@ function Square({ value, onSquareClick }) {
     </button>
   );
 }
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default function Board() {
+
+
+
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
     if (squares[i]) {
       return;
     }
@@ -27,8 +53,18 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+
+      <div className="status">{status}</div>  
       <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
